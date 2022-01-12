e81fe3691bd0da22e4877f92e6cb9539c68f78bd
AkshithD <Akshithd123@gmail.com>
2022-01-12 04:12:28 -0500

    Initial Commit

---
+++ Rutgers Eats/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  Rutgers Eats
+//
+//  Created by Akshith Dandemraju on 1/11/22.
+//
+
+import UIKit
+import SwiftUI
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
---
+++ Rutgers Eats/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rutgers Eats
+//
+//  Created by Akshith Dandemraju on 1/11/22.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+      
+        
+     
+                   
+        }
+        }
+            
+    
+
+    
+
+
