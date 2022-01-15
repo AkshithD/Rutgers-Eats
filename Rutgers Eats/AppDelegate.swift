2b16dbf1e24d5434be6c653cac15ef8229520f50
Akshith Dandemraju <90530645+AkshithD@users.noreply.github.com>
2022-01-15 02:25:52 -0500

    Update AppDelegate.swift

--- Rutgers Eats/AppDelegate.swift
+++ Rutgers Eats/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         
-        GMSPlacesClient.provideAPIKey("AIzaSyCIutsLcjoRiSQLylyVl6kx8xO2XE_CJaw")
+        GMSPlacesClient.provideAPIKey("API KEY")
     
         return true
     }
